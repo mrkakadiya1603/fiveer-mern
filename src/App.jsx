59d8184fcf453aa0ab6f41fc3c585aa2f54526ab
Jenil Kakadiya <jenilkakadiya6116@gmail.com>
2023-07-22 18:19:54 +0530

    Minor Changes .

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,68 @@
+import Footer from "./components/footer/Footer";
 import Navbar from "./components/navbar/Navbar"
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet
+} from "react-router-dom";
+import { Home } from "./pages/home/Home";
 
 function App() {
 
+  const Layout = ()=> {
+    return (
+      <div className="app">
+      <Navbar />
+      <Outlet />
+      <Footer />
+      </div>
+    )
+  }
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children:[
+        {
+        path:"/",
+        element: <Home />
+      },
+        {
+        path:"/",
+        element: <Home />
+      },
+        {
+        path:"/",
+        element: <Home />
+      },
+        {
+        path:"/",
+        element: <Home />
+      },
+        {
+        path:"/",
+        element: <Home />
+      },
+        {
+        path:"/",
+        element: <Home />
+      },
+        {
+        path:"/",
+        element: <Home />
+      },
+        {
+        path:"/",
+        element: <Home />
+      },
+    ]
+    },
+  ]);
+
   return (
     <div>
-     <Navbar />
+       <RouterProvider router={router} />
     </div>
   )
 }
